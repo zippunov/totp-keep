79f830e3bda4fd7e4c733621d1f603d227d078fd
Slava Zipp <zipp.pnl@gmail.com>
2017-10-25 10:10:38 +0300

    refectoring for encrypt

--- src/crpt.rs
+++ src/crpt.rs
@@ -1,7 +1,6 @@
 use errors::{Error};
 use utils::*;
 use rand::{OsRng, Rng};
-use crypto::bcrypt_pbkdf::bcrypt_pbkdf;
 use crypto::chacha20::ChaCha20;
 use crypto::symmetriccipher::SynchronousStreamCipher;
 use crypto::poly1305::Poly1305;
@@ -9,7 +8,8 @@ use crypto::mac::{Mac, MacResult};
 use std::ops::Range;
 use byteorder::{ByteOrder, BigEndian};
 
-pub struct RegistryFile {
+struct RegistryFile {
+    pub decrypted_len: usize,
     pub key_salt_len: usize,
     pub chacha_nonce_len: usize,
     pub tag_len: usize,
@@ -17,8 +17,9 @@ pub struct RegistryFile {
 }
 
 impl RegistryFile {
-    pub fn new() -> Self {
-        RegistryFile{
+    pub fn new_with_decrypted_length(len: usize) -> Self {
+        RegistryFile {
+            decrypted_len: len,
             key_salt_len: 16,
             chacha_nonce_len: 8,
             tag_len: 16,
@@ -26,39 +27,97 @@ impl RegistryFile {
         }
     }
 
-    pub fn key_salt(&self) -> Range<usize> {
+    pub fn new_with_encrypted_length(len: usize) -> Result<Self, Error> {
+        if len < 44 {
+            return Err(Error::CorruptedFileContent)
+        }
+        Ok(RegistryFile {
+            decrypted_len: len - 44,
+            key_salt_len: 16,
+            chacha_nonce_len: 8,
+            tag_len: 16,
+            bcrypt_pbkdf_cost: 16
+        })
+    }
+
+    pub fn encrypt(&self, body: &[u8], password: &str) -> Vec<u8> {
+        let mut content: Vec<u8> = vec![0u8; self.encrypted_len()];
+        self.fill_header(&mut content[..]);
+        let mut key: Vec<u8> = build_key(password, &content[self.key_salt()], self.bcrypt_pbkdf_cost);
+        self.encrypt_body(&key[..], &mut content[..], body);
+        self.fill_mac(&key[..], &mut content[..]);
+        content
+    }
+
+    pub fn decrypt(&self, encrypted: &Vec<u8>, password: &str) -> Result<Vec<u8>, Error> {
+        let key_salt = &encrypted[self.key_salt()];
+        let bcrypt_pbkdf_cost = BigEndian::read_u32(&encrypted[self.bcrypt_pbkdf()]);
+        let mut key: Vec<u8> = build_key(password, key_salt, bcrypt_pbkdf_cost);
+        let chacha20_key_range = ..32;
+        let poly1305_key_range = 32..64;
+        let body = &encrypted[self.body()];
+        let tag = &encrypted[self.tag()];
+        let mac = MacResult::new(&tag);
+        let mut poly1305 = Poly1305::new(&key[poly1305_key_range]);
+        poly1305.input(&body);
+        if !poly1305.result().eq(&mac) {
+            return Err(Error::WrongPassword);
+        }
+        let mut decrypted_body: Vec<u8> = vec![0u8; self.decrypted_len];
+        if self.decrypted_len > 0 {
+            let mut chacha = ChaCha20::new(&key[chacha20_key_range], &encrypted[self.chacha_nonce()]);
+            chacha.process(&body, &mut decrypted_body[..]);
+        }
+        Ok(decrypted_body)
+    }
+
+    fn fill_header(&self, content: &mut [u8]) {
+        let mut gen = OsRng::new().expect("Failed to get OS random generator");
+        gen.fill_bytes(&mut content[self.key_salt()]);
+        BigEndian::write_u32(&mut content[self.bcrypt_pbkdf()], self.bcrypt_pbkdf_cost);
+        gen.fill_bytes(&mut content[self.chacha_nonce()]);
+    }
+
+    fn encrypt_body(&self, key: &[u8], content: &mut [u8], input: &[u8]) {
+        if self.decrypted_len > 0 {
+            let mut chacha = ChaCha20::new(&key[..32], &content[self.chacha_nonce()]);
+            chacha.process(input, &mut content[self.body()]);
+        }
+    }
+
+    fn fill_mac(&self, key: &[u8], content: &mut [u8]) {
+        let mut poly1305 = Poly1305::new(&key[32..64]);
+        poly1305.input(&content[self.body()]);
+        let mac = poly1305.result();
+        copy_memory(mac.code(), &mut content[self.tag()]);
+    }
+
+    fn key_salt(&self) -> Range<usize> {
         (0..self.key_salt_len)
     }
 
-    pub fn bcrypt_pbkdf(&self) ->  Range<usize> {
+    fn bcrypt_pbkdf(&self) ->  Range<usize> {
         let start = self.key_salt().end;
         (start..(start + 4))
     }
 
-    pub fn chacha_nonce(&self) -> Range<usize> {
+    fn chacha_nonce(&self) -> Range<usize> {
         let start = self.bcrypt_pbkdf().end;
         (start..(start + self.chacha_nonce_len))
     }
 
-    pub fn body(&self, decrypted_len: usize) ->  Range<usize> {
+    fn body(&self) ->  Range<usize> {
         let start = self.chacha_nonce().end;
-        (start..(start + decrypted_len))
+        (start..(start + self.decrypted_len))
     }
 
-    pub fn tag(&self, decrypted_len: usize) ->  Range<usize> {
-        let start = self.body(decrypted_len).end;
+    fn tag(&self) ->  Range<usize> {
+        let start = self.body().end;
         (start..(start + self.tag_len))
     }
 
-    pub fn encrypted_len(&self, decrypted_len: usize) -> usize {
-        self.tag(decrypted_len).end
-    }
-
-    pub fn decrypted_len(&self, encrypted_len: usize) -> Result<usize, Error> {
-        if encrypted_len < (self.chacha_nonce().end + self.tag_len) {
-            return Err(Error::CorruptedFileContent);
-        }
-        Ok(encrypted_len - self.chacha_nonce().end - self.tag_len)
+    fn encrypted_len(&self) -> usize {
+        self.tag().end
     }
 }
 
@@ -71,55 +130,11 @@ fn build_key(password: &str, salt: &[u8], rounds: u32) -> Vec<u8> {
 }
 
 pub fn encrypt(body: &[u8], password: &str) -> Vec<u8> {
-    let decrypted_len = body.len();
-    let file = RegistryFile::new();
-    let mut result: Vec<u8> = vec![0u8; file.encrypted_len(decrypted_len)];
-
-    let mut gen = OsRng::new().expect("Failed to get OS random generator");
-    gen.fill_bytes(&mut result[file.key_salt()]);
-    BigEndian::write_u32(&mut result[file.bcrypt_pbkdf()], file.bcrypt_pbkdf_cost);
-    gen.fill_bytes(&mut result[file.chacha_nonce()]);
-
-    // key 64 bytes.
-    // - First 32 bytes makes key for the Chahca20,
-    // - Second 32 bytes makes key for poly1305
-    let mut key: Vec<u8> = build_key(password, &result[file.key_salt()], file.bcrypt_pbkdf_cost);
-    let chacha20_key_range = (..32);
-    let poly1305_key_range = (32..64);
-
-    if decrypted_len > 0 {
-        let mut chacha = ChaCha20::new(&key[chacha20_key_range], &result[file.chacha_nonce()]);
-        chacha.process(body, &mut result[file.body(decrypted_len)]);
-    }
-
-    let mut poly1305 = Poly1305::new(&key[poly1305_key_range]);
-    poly1305.input(&result[file.body(decrypted_len)]);
-    let mac = poly1305.result();
-    copy_memory(mac.code(), &mut result[file.tag(decrypted_len)]);
-    result
+    let file = RegistryFile::new_with_decrypted_length(body.len());
+    file.encrypt(body, password)
 }
 
 pub fn decrypt(encrypted: &Vec<u8>, password: &str) -> Result<Vec<u8>, Error> {
-    let file = RegistryFile::new();
-    let decrypted_len = file.decrypted_len(encrypted.len())?;
-    let key_salt = &encrypted[file.key_salt()];
-    let bcrypt_pbkdf_cost = BigEndian::read_u32(&encrypted[file.bcrypt_pbkdf()]);
-    let mut key: Vec<u8> = build_key(password, key_salt, bcrypt_pbkdf_cost);
-    let body = &encrypted[file.body(decrypted_len)];
-    let tag = &encrypted[file.tag(decrypted_len)];
-    let chacha20_key_range = (..32);
-    let poly1305_key_range = (32..64);
-
-    let mac = MacResult::new(&tag);
-    let mut poly1305 = Poly1305::new(&key[poly1305_key_range]);
-    poly1305.input(&body);
-    if !poly1305.result().eq(&mac) {
-        return Err(Error::WrongPassword);
-    }
-    let mut decrypted_body: Vec<u8> = vec![0u8; decrypted_len];
-    if decrypted_len > 0 {
-        let mut chacha = ChaCha20::new(&key[chacha20_key_range], &encrypted[file.chacha_nonce()]);
-        chacha.process(&body, &mut decrypted_body[..]);
-    }
-    Ok(decrypted_body)
+    let file = RegistryFile::new_with_encrypted_length(encrypted.len())?;
+    file.decrypt(encrypted, password)
 }
